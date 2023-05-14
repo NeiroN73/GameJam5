c1eb179330253b8577e2d272c8eadc71f11e5cba
NeiroN73 <sani4635@mail.ru>
2023-05-14 13:07:11 +0300

    Revert "Revert "add rules""
    This reverts commit 274f2638d757dee30a8b69469339241cfb6944ea.

--- Assets/Dev1/ScriptableObject/Cycles/CycleSettingsSO.cs
+++ Assets/Dev1/ScriptableObject/Cycles/CycleSettingsSO.cs
@@ -7,4 +7,8 @@ public class CycleSettingsSO : ScriptableObject
 {
     public int _cell;
     public int _time;
+    public int _humanCatchedAmount;
+    public int _disorderlyAmount;
+    public int _robotsAmount;
+    public int _humansAmount;
 }
--- Assets/Dev1/Scripts/Exit.cs
+++ Assets/Dev1/Scripts/Exit.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.TryGetComponent(out Human human)) 
+        if (collision.gameObject.TryGetComponent(out Human human))
         {
             CycleSwitcher.Instance.ActivateRestartPanel();
         }
